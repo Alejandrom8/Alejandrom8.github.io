5f2cbdd1ce2e4b8e16c0d8f819adf3d1a5726409
Alejandro Gómez García <ale.wild.ones@gmail.com>
2020-09-01 13:08:30 -0500

    Updates

---
+++ precache-manifest.20a6eaa4fb1af423b08a326dd5f03357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5464ea843dca1b4896a41513a43e3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c69008e737b81d6b05c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdd4749b9ea3d81671a8",
+    "url": "/static/js/2.945861e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.945861e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c69008e737b81d6b05c",
+    "url": "/static/js/main.4fb360ec.chunk.js"
+  },
+  {
+    "revision": "9bd35806b998045051a2",
+    "url": "/static/js/runtime-main.4a6ea0e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
